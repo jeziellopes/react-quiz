c97aaeac0a75726dc2d7039b38a4f67bb414edb4
jeziellopes <jeziellopes@gmail.com>
2022-06-03 18:46:15 -0300

    chore: add eslint

--- src/presentation/app/index.test.tsx
+++ src/presentation/app/index.test.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '.';
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+
+import App from '.'
 
 test('renders react quiz title', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/React Quiz/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/React Quiz/i)
+  expect(linkElement).toBeInTheDocument()
+})
