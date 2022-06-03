51a9769f6e3bab8b8d10d6bd1fba723ff08b4ad4
jeziellopes <jeziellopes@gmail.com>
2022-06-03 18:52:25 -0300

    feat: add data usecases

---
+++ src/data/usecases/load-questions.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@data/protocols'
+import { HttpStatusCode } from '@data/protocols/http'
+import { BadRequestError, InternalServerError, NotFoundError, UnexpectedError } from '@domain/errors'
+import { QuestionsLoader } from '@domain/usecases'
+
+export class RemoteQuestionsLoader implements QuestionsLoader {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient<QuestionsLoader.Model[]>
+  ) {}
+
+  async load (quizId: string): Promise<QuestionsLoader.Model[]> {
+    const httpResponse = await this.httpClient.request({
+      url: `${this.url}/${quizId}`,
+      method: 'post'
+    })
+
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: return httpResponse.body
+      case HttpStatusCode.badRequest: throw new BadRequestError()
+      case HttpStatusCode.notFound: throw new NotFoundError()
+      case HttpStatusCode.serverError: throw new InternalServerError()
+      default: throw new UnexpectedError()
+    }
+  }
+}
+
+export namespace RemoteQuestionsLoader {
+  export type Model = QuestionsLoader.Model
+}
