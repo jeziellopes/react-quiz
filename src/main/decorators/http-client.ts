7f9575e807c7a810f2a73e5c4cf2826826eacd2f
jeziellopes <jeziellopes@gmail.com>
2022-06-04 15:51:21 -0300

    feat: add main decorators

---
+++ src/main/decorators/http-client.ts
@@ -0,0 +1,11 @@
+import { HttpClient, HttpRequest, HttpResponse } from '@data/protocols'
+
+export class HttpClientDecorator implements HttpClient {
+  constructor (
+    private readonly httpClient: HttpClient
+  ) {}
+
+  async request (data: HttpRequest): Promise<HttpResponse> {
+    return this.httpClient.request(data)
+  }
+}
