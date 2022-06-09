aac552f0f18242e6d1c7ab50b69139f007bcb3d7
jeziellopes <jeziellopes@gmail.com>
2022-06-09 09:41:12 -0300

    feat: add presentation components

---
+++ src/presentation/components/structure/Button/styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100px;
+  height: 30px;
+  border: 1px #000 solid;
+  display: flex;
+  justify-content: center;
+`
+
+export const Button = styled.button`
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+  width: 100%;
+`
