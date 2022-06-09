50fce46edfcd8dcc6f1b7fe8eb388ffa2bb5647a
jeziellopes <jeziellopes@gmail.com>
2022-06-09 09:42:35 -0300

    feat: add presentation app and global styles

--- src/presentation/app/index.tsx
+++ src/presentation/app/index.tsx
@@ -1,6 +1,19 @@
+import { makeReduxStore } from '@main/factories/redux'
 import Routes from '@presentation/routes'
+import { GlobalStyle } from '@presentation/styles/global'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { ThemeProvider } from 'styled-components'
 
-const App = () => <Routes />
+const store = makeReduxStore()
+
+const App = () => (
+  <Provider store={store}>
+    <ThemeProvider theme={{}}>
+      <Routes />
+      <GlobalStyle />
+    </ThemeProvider>
+  </Provider>
+)
 
 export default App
